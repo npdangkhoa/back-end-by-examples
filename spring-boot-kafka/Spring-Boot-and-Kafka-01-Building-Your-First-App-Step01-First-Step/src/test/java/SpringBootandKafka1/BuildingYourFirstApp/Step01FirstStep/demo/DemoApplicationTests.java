0d13bf20ed83d153726ab4c7ead75fe06369060b
knguyen97 <knguyen97@dxc.com>
2019-05-04 15:09:52 +0700

    init kafka

---
+++ spring-boot-kafka/Spring-Boot-and-Kafka-01-Building-Your-First-App-Step01-First-Step/src/test/java/SpringBootandKafka1/BuildingYourFirstApp/Step01FirstStep/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package SpringBootandKafka1.BuildingYourFirstApp.Step01FirstStep.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
